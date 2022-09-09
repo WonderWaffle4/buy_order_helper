b1179d55f82f4e1b801a78d6a384bc54f11decd5
WonderWaffle4 <zhmelev.g@gmail.com>
2022-09-09 16:07:25 +0300

    Update get_marked_items_list.js

--- get_marked_items_list.js
+++ get_marked_items_list.js
@@ -26,24 +26,12 @@ function refreshButton() {
   refresh_button.style.border = 'none';
   let image_url = chrome.runtime.getURL('StyleImages/refresh16.png');
   refresh_button.innerHTML = '<img src=\"' + image_url + '\">';
-  let element = document.querySelectorAll(".panel-heading")[1];
+  let element = document.querySelectorAll(".panel-heading")[2];
   element.insertBefore(refresh_button, element.children[1]);
   refresh_button.append(element.children[2]);
 
   return refresh_button;
 }
-
-// refreshButtonOver = (event) => {
-//   let element = event.target;
-//   element.src = 'http://127.0.0.1:8887/refresh_active16.png';
-//   element.id = 'mouseover';
-// }
-
-// refreshButtonOut = (event) => {
-//   let element = event.target;
-//   element.src = 'http://127.0.0.1:8887/refresh16.png';
-//   element.id = 'mouseout';
-// }
 
 global_refresh_button_all.addEventListener("click", async () => {
   await getMarkedItemsList();
@@ -68,7 +56,7 @@ function refreshButtonAll() {
   refresh_button.style.border = 'none';
   let image_url = chrome.runtime.getURL('StyleImages/refresh_all16.png');
   refresh_button.innerHTML = '<img src=\"' + image_url + '\">';
-  let element = document.querySelectorAll(".panel-heading")[1];
+  let element = document.querySelectorAll(".panel-heading")[2];
   element.insertBefore(refresh_button, element.children[1]);
   refresh_button.append(element.children[2]);
 
@@ -90,18 +78,6 @@ global_refresh_button_all.children[0].addEventListener('mouseout', (event) => {
   let image_url = chrome.runtime.getURL('StyleImages/refresh_all16.png');
   element.src = image_url;
 });
-
-// refreshButtonAllOver = (event) => {
-//   let element = event.target;
-//   element.src = 'http://127.0.0.1:8887/refresh_all_active16.png';
-//   element.id = 'mouseover';
-// }
-
-// refreshButtonAllOut = (event) => {
-//   let element = event.target;
-//   element.src = 'http://127.0.0.1:8887/refresh_all16.png';
-//   element.id = 'mouseout';
-// }
 
 
 async function getMarkedItemsList() {
@@ -151,23 +127,11 @@ async function getMarkedItemsList() {
         marked_items = [[]];
         chrome.storage.local.set({ marked_items });
       });
-      //get key price
-      // let response = await fetch(currency_url);
-      // if (response.ok) console.log("ok");
-      // else console.log("error");
-      // let json = await response.json();
-      // const key_price = json.response.currencies.keys.price.value;
       let response = await fetchRetry(currency_url);
       let json = await response.json()
       key_price = json.response.currencies.keys.price.value;
       console.log(key_price);
 
-      //get user listings
-      // response = await fetch(user_buy_orders_url)
-      // if (response.ok) console.log("ok");
-      // else console.log("error");
-      // json = await response.json();
-      // const user_buy_orders = json.listings;
       response = await fetchRetry(user_buy_orders_url);
       json = await response.json()
       const user_buy_orders = json.listings;
@@ -177,79 +141,9 @@ async function getMarkedItemsList() {
           marked_items.push([]);
         }
         chrome.storage.local.set({ marked_items });
-        //console.log(marked_items);
       });
 
-      //let page_number = 1;
-      //let buy_order_number = 0;
-      //compare every user listing with global
-      // for (let user_buy_order of user_buy_orders) {
-      //   buy_order_number++;
-      //   if (buy_order_number % 12 == 0) {
-      //     page_number++;
-      //   }
-      //   let item_sku = user_buy_order.item.name;
-      //   buy_orders_url = "https://backpack.tf/api/classifieds/listings/snapshot?" + new URLSearchParams({
-      //     key: api_key,
-      //     sku: item_sku,
-      //     appid: '440',
-      //     token: token,
-      //   });
-      //   // response = await fetch(buy_orders_url)
-      //   // if (response.ok) console.log("ok");
-      //   // else console.log("buy order error:", response.error);
-      //   // json = await response.json();
-      //   // let buy_orders = json.listings;
-      //   console.log("1", item_sku);
-      //   response = await fetchRetry(buy_orders_url);
-      //   console.log("2", item_sku);
-      //   json = await response.json()
-      //   let buy_orders = json.listings;
-
-      //   for (let buy_order of buy_orders) {
-      //     if (buy_order.intent == 'buy') {
-
-
-      //       compareListings = (listing1, listing2, key_price) => {
-      //         let listing1_price = 0;
-      //         let listing2_price = 0;
-
-      //         if (listing1.keys != undefined) {
-      //           listing1_price += key_price * listing1.keys
-      //         };
-
-      //         if (listing1.metal != undefined) {
-      //           listing1_price += listing1.metal;
-      //         };
-
-      //         if (listing2.keys != undefined) {
-      //           listing2_price += key_price * listing2.keys
-      //         };
-
-      //         if (listing2.metal != undefined) {
-      //           listing2_price += listing2.metal;
-      //         };
-
-      //         return (listing1_price > listing2_price);
-      //       };
-
-
-      //       if (compareListings(buy_order.currencies, user_buy_order.currencies, key_price)) {
-      //         chrome.storage.sync.get("marked_items", ({ marked_items }) => {
-      //           marked_items[page_number - 1].push(['listing-' + user_buy_order.id, 1]);
-      //           chrome.storage.sync.set({ marked_items });
-      //         });
-      //       }
-      //       break;
-      //     };
-      //   };
-        
-      // };
-
       for (let i = 0; i < user_buy_orders.length; i++) {
-        // if (i % 10 == 0 && i != 0) {
-        //   page_number++;
-        // } 
         let item_sku = user_buy_orders[i].item.name;
         if (item_sku == '') 
           continue;
@@ -261,14 +155,7 @@ async function getMarkedItemsList() {
           appid: '440',
           token: token,
         });
-        // response = await fetch(buy_orders_url)
-        // if (response.ok) console.log("ok");
-        // else console.log("buy order error:", response.error);
-        // json = await response.json();
-        // let buy_orders = json.listings;
-        //console.log("1", item_sku);
         response = await fetchRetry(buy_orders_url);
-        //console.log("2", item_sku);
         json = await response.json()
         let buy_orders = json.listings;
 
@@ -315,14 +202,6 @@ async function getMarkedItemsList() {
         };
         
       };
-      
-      // .catch(() => {
-        //   chrome.storage.sync.get("marked_items", ({ marked_items }) => {
-        //     marked_items[page_number - 1].push(['listing-' + user_buy_order.id, 0]);
-        //     chrome.storage.sync.set({ marked_items });
-        //   });
-        //   return data;
-        // });
       console.log("ANALYZE DONE!!!!!!!!!!ANALYZE DONE!!!!!!!!!!ANALYZE DONE!!!!!!!!!!ANALYZE DONE!!!!!!!!!!");
       chrome.runtime.sendMessage("scanning done", (response) => { });
       return 0;
@@ -331,98 +210,3 @@ async function getMarkedItemsList() {
 
   return 1;
 }
-
-//get listings from current item and then compare them with user listings
-// // await fetch(buy_orders_url)
-// // .then((response) => {
-// //   if (response.ok) {
-// //     return response.json();
-// //   }
-// //   throw new Error('response was not okay');
-// // })
-// // .then((data) => {
-// //   buy_orders = data.listings;
-// //   for (let buy_order of buy_orders) {
-// //     if (buy_order.intent == 'buy') {
-
-
-// //       compareListings = (listing1, listing2, key_price) => {
-// //         let listing1_price = 0;
-// //         let listing2_price = 0;
-
-// //         if (listing1.keys != undefined) {
-// //           listing1_price += key_price * listing1.keys
-// //         };
-
-// //         if (listing1.metal != undefined) {
-// //           listing1_price += listing1.metal;
-// //         };
-
-// //         if (listing2.keys != undefined) {
-// //           listing2_price += key_price * listing2.keys
-// //         };
-
-// //         if (listing2.metal != undefined) {
-// //           listing2_price += listing2.metal;
-// //         };
-
-// //         return (listing1_price > listing2_price);
-// //       };
-
-
-// //       if (compareListings(buy_order.currencies, user_buy_order.currencies, key_price)) {
-// //         chrome.storage.sync.get("marked_items", ({ marked_items }) => {
-// //           marked_items[page_number - 1].push(['listing-' + user_buy_order.id, 1]);
-// //           chrome.storage.sync.set({ marked_items });
-// //         });
-
-// //       }
-// //       break;
-// //     };
-// //   };
-// //   return data;
-// // })
-// // .catch(() => {
-// //   chrome.storage.sync.get("marked_items", ({ marked_items }) => {
-// //     marked_items[page_number - 1].push(['listing-' + user_buy_order.id, 0]);
-// //     chrome.storage.sync.set({ marked_items });
-// //   });
-// //   return data;
-// // });
-
-// .then((response) => {
-      //   if (response.ok) {
-      //     return response.json();
-      //   }
-      //   throw new Error('Response was not ok.');
-      // })
-      // .then(async (data) => {
-      //   let user_buy_orders = data.listings;
-      //   let page_number = 1;
-      //   let buy_order_number = 0;
-      //   chrome.storage.sync.get("marked_items", ({ marked_items }) => {
-      //     for (let i = 0; i < Math.ceil(user_buy_orders.length / 10) - 1; i++) {
-      //       marked_items.push([]);
-      //     }
-      //     chrome.storage.sync.set({ marked_items });
-      //     console.log(marked_items);
-      //   });
-      //   for (let user_buy_order of user_buy_orders) {
-      //     buy_order_number++;
-      //     if (buy_order_number % 12 == 0) {
-      //       page_number++;
-      //     }
-      //     let item_sku = user_buy_order.item.name;
-      //     buy_orders_url = "https://backpack.tf/api/classifieds/listings/snapshot?" + new URLSearchParams({
-      //       key: api_key,
-      //       sku: item_sku,
-      //       appid: '440',
-      //       token: token,
-      //     });
-      //     //get listings from current item and then compare them with user listings
-      //     //
-      //     //
-      //     //
-      //   };
-      //   return data;
-      // });
